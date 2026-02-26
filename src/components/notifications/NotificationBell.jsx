16425a9430cc23698394128a758daa03806a0e82
shounak.sarker <shounaksarker@gmail.com>
2026-02-26 09:52:19 +0600

    fixed bell icon

--- src/components/notifications/NotificationBell.jsx
+++ src/components/notifications/NotificationBell.jsx
@@ -4,6 +4,7 @@ import React, { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 import { GET_UNREAD_COUNT_URL } from '@/helpers/frontend/apiEndpoints';
 import NotificationPanel from './NotificationPanel';
+import Bell from '@/assets/svg/Icon/Bell';
 
 const POLL_INTERVAL = 120000; // 120 seconds
 
@@ -49,19 +50,7 @@ const NotificationBell = () => {
         title="Notifications"
       >
         {/* Bell Icon SVG */}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="size-5"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" />
-          <path d="M13.73 21a2 2 0 0 1-3.46 0" />
-        </svg>
+        <Bell />
 
         {/* Unread Badge */}
         {unreadCount > 0 && (
